5b9e776543ff95a996017b888537195b31cb5ae7
AndresBo <anndresfborrero@gmail.com>
2023-06-21 22:16:30 +1000

    build: added comment when filter search has no matching names

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,14 +1,16 @@
 const Persons = ({persons, filterName}) => {
-    console.log('filterName state: ',filterName)
-    const filteredPersons = persons.filter(person => person.name.includes(filterName))
-    console.log('filtered persons: ', filteredPersons)
-    console.log('filtered persons length: ', filteredPersons.length)
     
-    return (
+    const filteredPersons = persons.filter(person => person.name.includes(filterName))
+    
+    if (filteredPersons.length === 0) {
+      return <p>No matches</p>
+    } else {
+      return (
         <ul>
-            {filteredPersons.map(person => <li key={person.id}>{person.name}</li>)}
+          {filteredPersons.map(person => <li key={person.id}>{person.name}</li>)}
         </ul>
-    )
+      )
+    }
 }
 
 export default Persons
