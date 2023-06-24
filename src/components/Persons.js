bd29ca4adf9b64a6c1a4b9ad2de1fcb5beca7711
AndresBo <anndresfborrero@gmail.com>
2023-06-24 20:42:39 +1000

    build: created delete button for Person component

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -8,7 +8,8 @@ const Persons = ({persons, filterName}) => {
     } else {
       return (
         <ul>
-          {filteredPersons.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
+          {filteredPersons.map(person => 
+            <li key={person.id}>{person.name} {person.number} <button>delete</button></li>)}
         </ul>
       )
     }
